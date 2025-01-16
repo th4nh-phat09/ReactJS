e42041c8c0150f1cfbc193553e7134825a25c0ef
th4nh-phat09 <thanphat354@gmail.com>
2025-01-16 23:16:12 +0700

    2 way biding checkbox

--- src/App.js
+++ src/App.js
@@ -7,50 +7,28 @@ function App() {
     { id: '2', text: 'JavaScript' },
     { id: '3', text: 'ReactJS' },
   ];
-  const [checked, setChecked] = useState('1'); // Lưu trữ ID của radio button được chọn
+  const [checked, setChecked] = useState([]); // Lưu trữ ID của radio button được chọn
 
   const handleClick = (id) => {
-      setChecked(id);
+     if(checked.includes(id)) {
+      setChecked(checked.filter(item => item !== id));
   }
+  else
+    setChecked(prev => [...prev, id]);
+}
 
   return (
     <div>
     <h1>Chọn Khóa Học</h1>
       {courses.map(course =>
         <div key={course.id}>
-          <input type="radio" checked={checked === course.id} onChange={()=>handleClick(course.id)}/>
+          <input type="Checkbox" checked={checked.includes(course.id)} onChange={()=>handleClick(course.id)}/>
           <label htmlFor={course.id}>{course.text}</label>
         </div>
       )}
     </div>
   )
 
-  // const [checked, setChecked] = useState(''); // Lưu trữ ID của radio button được chọn
-
-  // // Hàm xử lý khi người dùng chọn một radio button
-  // function handleChange(id) {
-  //   setChecked(id); // Cập nhật `checked` với `id` được chọn
- // }
-
-  // return (
-  //   <div>
-  //     <h1>Chọn khóa học</h1>
-  //     {courses.map(course => (
-  //       <div key={course.id}>
-  //         <input
-  //           type="radio"
-  //           id={course.id}
-  //           name="course"
-  //           value={course.id}
-  //           checked={checked === course.id}
-  //           onChange={() => handleChange(course.id)} // Gọi hàm handleChange khi chọn
-  //         />
-  //         <label htmlFor={course.id}>{course.text}</label>
-  //       </div>
-  //     ))}
-  //     <p>Khóa học đã chọn: {checked}</p>
-  //   </div>
-  // );
 }
 
 export default App;
